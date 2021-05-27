205f50bc6412cc29a9a52eda11b04349c4015138
kevin <765002108@qq.com>
2021-05-27 16:45:19 +0800

    提交修改

--- asm/src/main/kotlin/com/sample/asm/LogMethodVisitor.kt
+++ asm/src/main/kotlin/com/sample/asm/LogMethodVisitor.kt
@@ -2,7 +2,7 @@ package com.sample.asm
 import org.objectweb.asm.MethodVisitor
 import org.objectweb.asm.Opcodes
 
-class TimerMethodVisitor (methodVisitor: MethodVisitor):MethodVisitor(Opcodes.ASM5, methodVisitor){
+class LogMethodVisitor (methodVisitor: MethodVisitor):MethodVisitor(Opcodes.ASM5, methodVisitor){
 
     override fun visitCode() {
         super.visitCode()
